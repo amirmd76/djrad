048e477a9fee8e92f02a594fbf999021529a20ba
AmirMohammad Dehghan <amir@divar.ir>
2017-07-22 13:01:36 +0430

    ready for publishing

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import setup, find_packages
 
-
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+readme = "Django-JSONRPC-REST-API-Decorators (DJRAD)"
 
 setup(
     name='djrad',
-    version='0.1.2',
+    version='1.0.0',
     description='Decorators for JSONRPC and REST API on django',
     long_description=readme,
     author='Divar submit team',
     author_email='rteam@divar.ir',
     url='https://github.com/amirmd76/djrad.git',
-    license=license,
+    license='MIT',
     packages=find_packages(),
     install_requires=[
           'django', 'jsonschema==2.6.0'
