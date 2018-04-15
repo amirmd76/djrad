020de28bdd53aed79034e76f77306a640bd8238b
AmirMohammad Dehghan <amirmd76@gmail.com>
2018-04-15 17:14:39 -0400

    new build

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     version='1.2.1',
     description='Decorators for JSONRPC and REST API on django',
     long_description=readme,
-    author='Divar submit team',
-    author_email='rteam@divar.ir',
+    author='Divar submit team - AmirMohammad Dehghan',
+    author_email='dehghan@mit.edu',
     url='https://github.com/amirmd76/djrad.git',
     license='MIT',
     packages=find_packages(),
     install_requires=[
-          'django', 'jsonschema==2.6.0'
+          'django', 'jsonschema>=2.6.0'
       ]
 )
